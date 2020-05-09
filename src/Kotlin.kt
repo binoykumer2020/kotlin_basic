a8e2b62e6d3d1b765be1604829db936b821cbed6
Ishaan Arya Gupta <ishaan@gmail.com>
2020-05-09 19:59:23 +0100

    my first program

---
+++ src/Kotlin.kt
@@ -0,0 +1,5 @@
+class Kotlin {
+
+    var name : String = ""
+
+}
--- src/MyFirstKotlinProgram.kt
+++ src/MyFirstKotlinProgram.kt
@@ -6,4 +6,11 @@ fun main(args : Array<String>)
     var a =10
     var b = 20;
     println(a+b)
+
+    var Binoy = Kotlin()
+    Binoy.name = "First Class"
+    println("The given name is :" + Binoy.name + " and I am really enjoying it")
+    // another method to print
+    println("The another way: ${Binoy.name}" + " so cool")
+
 }
