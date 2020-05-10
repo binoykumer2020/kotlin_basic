5389bba9a1344146613e2c23dd9b30993febc785
binoykumer2020 <binoykdasgupta@icloud.com>
2020-05-10 14:40:33 +0100

    my second program push

--- src/control_flow/IteratorOperator.kt
+++ src/control_flow/IteratorOperator.kt
@@ -1,2 +1,24 @@
+/*
+Iterator: we can define our own iterator in our class by implementing iterator operator
+1. define iterator in the class. It must be named iterator and have the operator modifier
+2. returns next() and hasNext()
+ */
+
+
 package control_flow
 
+class Animal (val name: String)
+class Zoo (val animals: List<Animal>)
+{
+    operator fun iterator (): Iterator<Animal>
+    {
+        return animals.iterator()
+    }
+}
+
+fun main(args: Array<String>) {
+    val zoo = Zoo(listOf(Animal("tiger"), Animal("lion"), Animal("zebra")))
+
+    for (animal in zoo){
+        println("watch out, it's a ${animal.name}") }
+}
--- src/control_flow/loops.kt
+++ src/control_flow/loops.kt
@@ -1,15 +1,39 @@
 package control_flow
 
+import sun.java2d.IntegerNIORaster
+
 fun main(args: Array<String>) {
 
-    for (i in 1 .. 10)
+    /*
+    for (i in 1..10)
     { println(i) }
 
-    val cakes = listOf("carrot", "Cheese", "vanilla", "Chocolate" )
-    for (cake in cakes)
-    {
-
-        println("Yummy, it's a $cake cake!")
-
+    val cakes = listOf("carrot", "Cheese", "vanilla", "Chocolate")
+    for (cake in cakes) {
+         println("Yummy, it's a $cake cake!")
     }
+    *
+
+    // we can think of ranges as well
+
+     for ( i in 0 .. 5){
+        // println(i)
+     }
+
+    for (i in 1..10 step 2)
+     //  { println(i) }
+
+    for (i in 5 downTo 1)  {println(i)}
+
+     */
+
+     for ( character in 'a' .. 'z')
+      //  {  println(character)  }
+
+     println("Enter a number between 0 to 100")
+     var number = Integer.valueOf(readLine())
+
+    
+
+
 }
