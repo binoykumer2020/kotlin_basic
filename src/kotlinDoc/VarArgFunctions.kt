4e4a00126cb729fc5ce4fdbfe4970d185717752f
binoykumer2020 <binoykdasgupta@icloud.com>
2020-05-10 11:30:01 +0100

    my second program push

--- src/kotlinDoc/VarArgFunctions.kt
+++ src/kotlinDoc/VarArgFunctions.kt
@@ -5,21 +5,19 @@ fun main(args: Array<String>) {
     // the vararg modifier turns a parameter into vararg
     // this allows printall with any number of strings arguments
 
-    fun printAll ( vararg messages : String)
-    {
-        for ( i in messages)
+    fun printAll(vararg messages: String) {
+        for (i in messages)
             println(i)
     }
     printAll("123", "Hello", "Vamos", "Nmanastee")
 
     // with prefix
 
-    fun printAllWithPrefix (vararg messages : String, prefix : String )
-    {
-        for ( m in messages)
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {
+        for (m in messages)
             println(prefix + m)
     }
     printAllWithPrefix(" Hello ", " Ishaan", " Namastee",
-    prefix = "Greetings")
+            prefix = "Greetings")
 
 }
